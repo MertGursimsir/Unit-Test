4053fee8f9ad11b50c395f34beaf67050965fbad
Mert Gürşimşir <mertgursimsir16@gmail.com>
2025-11-10 15:44:53 +0300

    new tests added to checkout - hp

--- 6-SUPERMARKET/Checkout.py
+++ 6-SUPERMARKET/Checkout.py
@@ -1,9 +1,52 @@
 class Checkout:
+    class Discount:
+        # Take nbrItems of item, pay the price
+        def __init__(self, nbrItems, price):
+            self.nbrItems = nbrItems
+            self.price = price
+
+    def __init__(self):
+        self.prices = {}
+        self.discounts = {}
+        self.items = {}
+
+    def addDiscount(self, item, nbrOfItems, price):
+        discount = self.Discount(nbrOfItems, price)
+        self.discounts[item] = discount
+
     def addItemPrice(self, item, price):
-        pass
+        self.prices[item] = price
 
     def addItem(self, item):
-        pass
+        if item not in self.prices.keys():
+            raise Exception("Bad Item")
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
 
     def calculateTotal(self):
-        return 1
+        total = 0
+        for item, count in self.items.items():
+            total += self.calculateItemTotal(item, count)
+        return total
+
+    def calculateItemTotal(self, item, count):
+        total = 0
+        if item in self.discounts:
+            discount = self.discounts[item]
+            if count >= discount.nbrItems:
+                total += self.calculateItemDiscountedTotal(item, count, discount)
+            else:
+                total += self.prices[item] * count
+        else:
+            total += self.prices[item] * count
+        return total
+
+    def calculateItemDiscountedTotal(self, item, count, discount):
+        total = 0
+        nbrOfDiscounts = count / discount.nbrItems
+        total += nbrOfDiscounts * discount.price
+        remaining = count % discount.nbrItems
+        total += remaining * self.prices[item]
+        return total
--- 6-SUPERMARKET/TestCheckout.py
+++ 6-SUPERMARKET/TestCheckout.py
@@ -7,6 +7,8 @@ from Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
     return checkout
 
 # def test_CanAddItemPrice(checkout):
@@ -16,6 +18,25 @@ def checkout():
 #     checkout.addItem("a")
 
 def test_CanCalculateTotal(checkout):
-    checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
+
+def test_GetCorrectTotalWithMultipleItems(checkout):
+    checkout.addItem("a")
+    checkout.addItem("b")
+    assert checkout.calculateTotal() == 3
+
+def test_canAddDiscountRule(checkout):
+    checkout.addDiscount("a", 3, 2)
+
+# @pytest.mark.skip # Skips the test
+def test_canApplyDiscountRule(checkout):
+    checkout.addDiscount("a", 3, 2)
+    checkout.addItem("a")
+    checkout.addItem("a")
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 2
+
+def test_ExceptionWithBadItem(checkout):
+    with pytest.raises(Exception):
+        checkout.addItem("c")
